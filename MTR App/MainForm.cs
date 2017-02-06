c2d3541877353433c9a072493298ee56995ca934
George McClain <McClainG9616@Gmail.com>
2017-02-06 14:30:49 -0500

    clean up

--- MTR App/MainForm.cs
+++ MTR App/MainForm.cs
@@ -1055,7 +1055,6 @@ namespace MTR_APP
                     //AutoSize Datagrid Rows and Colums to fit the Datagrid
                     dgJobGridBun.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
                     dgJobGridBun.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
-
                 }
             }
             catch (Exception ex)
@@ -1187,95 +1186,94 @@ namespace MTR_APP
 
         private void Form1_Load(object sender, EventArgs e)
         {
-                zManufacturerCombo();
-                zMillLocationCombo();
-                zProductDescriptionCombo();
-                zWeldSeamTypeCombo();
-                zOuterDimensionCombo();
-                zWallThicknessCombo();
-                zCoatingCombo();
-                zGradeCombo();
-                zAnsiAsmeCombo();
-                zStandardCombo();
-                try
+            zManufacturerCombo();
+            zMillLocationCombo();
+            zProductDescriptionCombo();
+            zWeldSeamTypeCombo();
+            zOuterDimensionCombo();
+            zWallThicknessCombo();
+            zCoatingCombo();
+            zGradeCombo();
+            zAnsiAsmeCombo();
+            zStandardCombo();
+            try
+            {
+                SqlConnection con = new SqlConnection(Connection.MTRDataBaseConn);
                 {
-                    SqlConnection con = new SqlConnection(Connection.MTRDataBaseConn);
-                    {
-                        con.Open();
-                        SqlCommand cmd = new SqlCommand();
-                        cmd.CommandText = "SELECT * FROM dbo.[MasterTable]";
-                        cmd.Connection = con;
+                    con.Open();
+                    SqlCommand cmd = new SqlCommand();
+                    cmd.CommandText = "SELECT * FROM dbo.[MasterTable]";
+                    cmd.Connection = con;
 
-                        //DataAdapter
-                        masterDA = new SqlDataAdapter(cmd.CommandText, con);
+                    //DataAdapter
+                    masterDA = new SqlDataAdapter(cmd.CommandText, con);
 
-                        //MySqlCommand
-                        SqlCommand myCMD = new SqlCommand(cmd.CommandText, con);
+                    //MySqlCommand
+                    SqlCommand myCMD = new SqlCommand(cmd.CommandText, con);
 
-                        //DataAdapter to Command
-                        masterDA.SelectCommand = myCMD;
+                    //DataAdapter to Command
+                    masterDA.SelectCommand = myCMD;
 
-                        //Define Datatable
-                        masterDT = new DataTable();
+                    //Define Datatable
+                    masterDT = new DataTable();
 
-                        //Command Builder (IS GOD!)
-                        SqlCommandBuilder cb = new SqlCommandBuilder(masterDA);
+                    //Command Builder (IS GOD!)
+                    SqlCommandBuilder cb = new SqlCommandBuilder(masterDA);
 
-                        //Teach Command builder to be a boss!
-                        masterDA.UpdateCommand = cb.GetUpdateCommand();
-                        masterDA.InsertCommand = cb.GetInsertCommand();
-                        masterDA.DeleteCommand = cb.GetDeleteCommand();
+                    //Teach Command builder to be a boss!
+                    masterDA.UpdateCommand = cb.GetUpdateCommand();
+                    masterDA.InsertCommand = cb.GetInsertCommand();
+                    masterDA.DeleteCommand = cb.GetDeleteCommand();
 
-                        //Fill the DataTable with DataAdapter information
-                        masterDA.Fill(masterDT);
+                    //Fill the DataTable with DataAdapter information
+                    masterDA.Fill(masterDT);
 
-                        //Fill DataTable with Database Schema
-                        masterDA.FillSchema(masterDT, SchemaType.Source);
+                    //Fill DataTable with Database Schema
+                    masterDA.FillSchema(masterDT, SchemaType.Source);
 
-                        //Bind The Data Table to the DataGrid
-                        dgMasterGridBun.DataSource = masterDT;
+                    //Bind The Data Table to the DataGrid
+                    dgMasterGridBun.DataSource = masterDT;
 
-                        //AutoSize Datagrid Rows and Colums to fit the Datagrid
-                        dgMasterGridBun.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
-                    }
-                }
-                //Catch Exception
-                catch (Exception ex)
-                {
-                    MessageBox.Show(this, ex.Message, "SQL ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                }
-
-                //////////////////////////////////////////////////////////////////////////
-                /////////////////////      Populate Job Name ComboBox    /////////////////
-                //////////////////////////////////////////////////////////////////////////
-
-                //Select Table
-                try
-                {
-                    using (SqlConnection con = new SqlConnection(Connection.MTRDataBaseConn))
-                    {
-                        con.Open();
-                        SqlCommand cmd = new SqlCommand();
-                        cmd.CommandText = "SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES";
-                        cmd.Connection = con;
-
-                        using (SqlDataReader reader = cmd.ExecuteReader())
-                        {
-                            cmbJobName.Items.Clear();
-                            while (reader.Read())
-                            {
-                                cmbJobName.Items.Add((string)reader["TABLE_NAME"]);
-                            }
-                        }
-                        con.Close();
-                    }
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show(ex.Message);
+                    //AutoSize Datagrid Rows and Colums to fit the Datagrid
+                    dgMasterGridBun.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
                 }
             }
-        
+            //Catch Exception
+            catch (Exception ex)
+            {
+                MessageBox.Show(this, ex.Message, "SQL ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            //////////////////////////////////////////////////////////////////////////
+            /////////////////////      Populate Job Name ComboBox    /////////////////
+            //////////////////////////////////////////////////////////////////////////
+
+            //Select Table
+            try
+            {
+                using (SqlConnection con = new SqlConnection(Connection.MTRDataBaseConn))
+                {
+                    con.Open();
+                    SqlCommand cmd = new SqlCommand();
+                    cmd.CommandText = "SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES";
+                    cmd.Connection = con;
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        cmbJobName.Items.Clear();
+                        while (reader.Read())
+                        {
+                            cmbJobName.Items.Add((string)reader["TABLE_NAME"]);
+                        }
+                    }
+                    con.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
 
         #region EnterKeyDownSubmit
 
@@ -2098,7 +2096,6 @@ namespace MTR_APP
                     //AutoSize Datagrid Rows and Colums to fit the Datagrid
                     dgJobGridBun.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
                     dgJobGridBun.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
-
                 }
             }
             catch (Exception ex)
